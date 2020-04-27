a31dfd6663b8b51b0291850c596d8b12de0d8513
Arthur Lima <arthur-esc@hotmail.com>
2020-04-27 09:47:25 -0300

    Initial commit :dancer:

---
+++ lib/cards.ex
@@ -0,0 +1,10 @@
+defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values, suit <- suits do
+      "#{value} of #{suit}"
+    end
+  end
+end
